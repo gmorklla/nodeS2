ba70af1b4bbf0ea2949880270439b9f811052838
gmorklla <gmorklla@gmail.com>
2019-10-24 11:58:14 -0500

    Node express server

---
+++ routes/download.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+router.all('/*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, responseType');
+  next();
+});
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.sendFile(path.resolve('./public/files/edoCta.pdf'));
+});
+
+module.exports = router;
